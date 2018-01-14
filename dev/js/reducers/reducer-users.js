06609a640e5cba355406f5529fdb48297ea87c2a
Sikindar Mirza <sikindar@enroco.com>
2018-01-14 11:11:11 +0530

    created a users reducer

---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,29 @@
+export default function {
+  rerurn  [
+    {
+      id: 1,
+      first: Sikindar,
+      last: Mirza,
+      age: 26,
+      description: "Sikki is a fullstack developer",
+      thumbnail:
+    },
+    {
+      id: 2,
+      first: Dinesh,
+      last: Pallapa,
+      age: 23,
+      description: "Dani is a fullstack developer",
+      thumbnail:
+    },
+    {
+      id: 3,
+      first: Miya,
+      last: Mirza,
+      age: 23,
+      description: "Miya is a fullstack developer",
+      thumbnail:
+    }
+  ]
+
+}
