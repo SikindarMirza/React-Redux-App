7707d2eeea54b15961045aadfef445ab13ec659f
Sikindar Mirza <sikindar@enroco.com>
2018-01-14 16:03:34 +0530

    created a app component and added a provider

--- dev/js/reducers/reducer-users.js
+++ dev/js/reducers/reducer-users.js
@@ -2,24 +2,24 @@ export default function() {
   return [
     {
       id: 1,
-      first: Sikindar,
-      last: Mirza,
+      first: "Sikindar",
+      last: "Mirza",
       age: 26,
       description: "Sikki is a fullstack developer",
       thumbnail: "sikki"
     },
     {
       id: 2,
-      first: Dinesh,
-      last: Pallapa,
+      first: "Dinesh",
+      last: "Pallapa",
       age: 23,
       description: "Dani is a fullstack developer",
       thumbnail: "dani"
     },
     {
       id: 3,
-      first: Miya,
-      last: Mirza,
+      first: "Miya",
+      last: "Mirza",
       age: 23,
       description: "Miya is a fullstack developer",
       thumbnail: "miya"
